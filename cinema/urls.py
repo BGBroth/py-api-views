19ec03f44d6ef1bf4458c7b7c172c15b789eaa66
rivrxsq <shevchenkovika01@gmail.com>
2025-01-12 01:27:49 +0200

    add urls

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,13 +1,25 @@
-from django.urls import path, include
-from rest_framework import routers
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+from cinema.views import (
+    MovieViewSet,
+    GenreList,
+    GenreDetail,
+    CinemaHallList,
+    CinemaHallDetail,
+    ActorList,
+    ActorDetail
+)
 
-from cinema.views import MovieViewSet
-
-router = routers.DefaultRouter()
-router.register("buses", MovieViewSet)
-
-urlpatterns = [
-    path("", include(router.urls)),
-]
+router = DefaultRouter()
+router.register("movies", MovieViewSet, basename="movie")
 
 app_name = "cinema"
+
+urlpatterns = [
+    path("genres/", GenreList.as_view(), name="genre-list"),
+    path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
+    path("actors/", ActorList.as_view(), name="actor-list"),
+    path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
+    path("cinema_halls/", CinemaHallList.as_view(), name="actor-list"),
+    path("cinema_halls/<int:pk>/", CinemaHallDetail.as_view(), name="actor-detail"),
+] + router.urls
